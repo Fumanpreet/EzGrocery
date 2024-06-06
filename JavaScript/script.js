b79a8aff25fb0e79edac0ceedd6af8dd63976ea6
Fumanpreet Singh <fumanpre@ualberta.ca>
2024-06-05 21:32:49 -0600

    created cart and added few javascript features

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -5,7 +5,7 @@ changeSliderPoster = () => {
     slider_right_button.click();
 }
 
-setInterval(changeSliderPoster, 4000);
+setInterval(changeSliderPoster, 5000);
 
 
 // change the best deals of the day (MANUALLY)
@@ -19,9 +19,9 @@ let best_deal_left_swipe = document.getElementById("best-deal-left-button");
 best_deal_left_swipe.addEventListener("click", function () {
 
     all_Best_Deals_Poster[current_best_deals_displayNo].className = "best-deal-poster disappear";
-    current_best_deals_displayNo = (current_best_deals_displayNo - 1) % best_deals_length;    
+    current_best_deals_displayNo = (current_best_deals_displayNo - 1) % best_deals_length;
 
-    if (current_best_deals_displayNo < 0){
+    if (current_best_deals_displayNo < 0) {
         current_best_deals_displayNo = best_deals_length + current_best_deals_displayNo;
     }
     console.log(current_best_deals_displayNo);
@@ -48,3 +48,55 @@ changeBestDealPoster = () => {
 
 
 setInterval(changeBestDealPoster, 4000);
+
+
+// scroll the arrival item 
+let arrivalWindows = document.querySelectorAll(".arrival-items-body");
+
+let scrollArrival = () => {
+    arrivalWindows.forEach(arrivalWindow => {
+        let scrollAmount = 155;
+        let maxScrollLeft = arrivalWindow.scrollWidth - arrivalWindow.clientWidth;
+
+        if (arrivalWindow.scrollLeft + scrollAmount >= maxScrollLeft) {
+            // Scroll to the start
+            arrivalWindow.scrollTo({
+                left: 0,
+                behavior: 'smooth'
+            });
+        } else {
+            // Scroll by the specified amount
+            arrivalWindow.scrollBy({
+                left: scrollAmount,
+                behavior: 'smooth'
+            });
+        }
+    });
+}
+
+setInterval(scrollArrival, 4000);
+
+
+// scroll the stores 
+let storeWindow = document.querySelector(".cards");
+
+let scrollStore = () => {
+    let scrollAmount = 172.5;
+    let maxScrollLeft = storeWindow.scrollWidth - storeWindow.clientWidth;
+
+    if (storeWindow.scrollLeft + scrollAmount >= maxScrollLeft) {
+        // Scroll to the start
+        storeWindow.scrollTo({
+            left: 0,
+            behavior: 'smooth'
+        });
+    } else {
+        // Scroll by the specified amount
+        storeWindow.scrollBy({
+            left: scrollAmount,
+            behavior: 'smooth'
+        });
+    }
+}
+
+setInterval(scrollStore, 5000);
